0bcd37f8ee600bbd6028826f04668d8ddad7a5d8
Martin Babka <babka@atteq.com>
2017-06-09 15:23:54 +0200

    Test commit for read the docs webhook

--- tests/test_test.py
+++ tests/test_test.py
@@ -4,3 +4,5 @@ from unittest import TestCase
 class MyTestCase(TestCase):
     def test_it(self):
         self.assertEquals(True, True, "Should be OK")
+        self.assertEquals(True, True, "Should be OK2")
+
