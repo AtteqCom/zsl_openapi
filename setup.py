733046827e03326d888aa770bbfdc5e9c77871bc
Martin Babka <babka@atteq.com>
2017-06-22 18:23:53 +0200

    Allow automatic pypi deployment

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='zsl-openapi',
-    version='0.1.1',
+    version='0.1.2',
     license='BSD',
     description='Generate OpenAPI specification out of your ZSL service.',
     long_description='%s\n%s' % (
